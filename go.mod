f4506b1bbb38a22b90013f763be7dbe65529bda1
yajium <yajiuma720@gmail.com>
2023-04-14 23:30:25 +0900

    module and package

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_study
 
 go 1.20
+
+require github.com/yajium/calculator v0.0.0
+
+// リモートではなく、ローカルディレクトリを使用するようにreplace
+replace github.com/yajium/calculator => ../calculator
--- main.go
+++ main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"fmt"
 	"math/rand"
+
+	"github.com/yajium/calculator"
 )
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
+
+	// パッケージの使用
+	total := calculator.Sum(3, 5)
+	fmt.Println((total))
+	fmt.Println("Version: ", calculator.Version)
 }
