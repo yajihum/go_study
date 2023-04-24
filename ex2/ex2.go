e8a914912c551de3fc35777762c1afa2fd57180a
yajium <yajiuma720@gmail.com>
2023-04-25 00:06:52 +0900

    fix: ex2

--- ex2/ex2.go
+++ ex2/ex2.go
@@ -5,17 +5,28 @@ import (
 )
 
 func getFibonacciSequence(val int) []int {
-	var nums []int
+	//var nums []int
 	if val < 2 {
-		return nums // 何も入っていない状態であればnilを暗黙的に返せる
+		//return nums  何も入っていない状態であればnilを暗黙的に返せる
+		return make([]int, 0) // リファクタリング
 	}
 
-	for i := 0; i < val; i++ {
-		if i <= 1 {
-			nums = append(nums, 1)
-			continue
-		}
-		nums = append(nums, nums[i-1]+nums[i-2])
+	// リファクイズ
+	// makeでスライスを作成　:=との違いは要素数を指定しなくていい
+	nums := make([]int, val)
+	nums[0], nums[1] = 1, 1
+
+	// for i := 0; i < val; i++ {
+	// 	if i <= 1 {
+	// 		nums = append(nums, 1)
+	// 		continue
+	// 	}
+	// 	nums = append(nums, nums[i-1]+nums[i-2])
+	// }
+
+	// リファクタリング
+	for i := 2; i < val; i++ {
+		nums[i] = nums[i-1] + nums[i-2]
 	}
 	return nums
 }
