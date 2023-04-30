01637341d9ab4d19ef57f28e62d04b5530bc027e
yajium <yajiuma720@gmail.com>
2023-04-30 13:37:55 +0900

    feat: interface

--- interface/interface.go
+++ interface/interface.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"math"
+	"net/http"
+	"os"
 )
 
 // インターフェースの定義
@@ -51,10 +55,55 @@ func printInformation(s Shape) {
 	fmt.Println()
 }
 
+type Person struct {
+	Name, Country string
+}
+
+// カスタムでStringメソッドを作れる
+func (p Person) String() string {
+	// Sprintfはstring型とそれ以外の型（今回の場合Person型）を
+	// 一緒にstring型にして出力してくれるもの
+	return fmt.Sprintf("%v is from %v", p.Name, p.Country)
+}
+
+// 空の構造体
+type customWriter struct{}
+
+type GitHubResponse []struct {
+	FullName string `json:"full_name"`
+}
+
+// p：APIからとってきたJSONデータからfull_nameのところだけ取得して
+func (w customWriter) Write(p []byte) (n int, err error) {
+	var resp GitHubResponse
+	json.Unmarshal(p, &resp)
+	// rangeはsliceとかを回せるやつ
+	// 最初の変数の方にはindexが取れるが今回は不要
+	for _, r := range resp {
+		fmt.Println(r.FullName)
+	}
+	return len(p), nil
+}
+
 func main() {
 	var s Shape = Square{3}
 	printInformation(s)
 
 	c := Circle{6} // Shapeオブジェクトと指定してなくてもいける
 	printInformation(c)
+
+	rs := Person{"John Doe", "USA"}
+	ab := Person{"Mark Collins", "United Kingdom"}
+	fmt.Printf("%s\n%s\n", rs, ab)
+
+	// Github APIを使用してMicrosoftから3つリポジトリを取得する
+	resp, err := http.Get("https://api.github.com/users/microsoft/repos?page=15&per_page=5")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	writer := customWriter{}
+	// 最初の引数にWriterがあるioの中身を見るとWriterインターフェースがある
+	io.Copy(writer, resp.Body)
 }
